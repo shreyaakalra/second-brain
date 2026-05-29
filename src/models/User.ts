bb9b38d8084d39a5abb143c343f7df17e0596e9c
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-05-30 01:33:39 +0530

    User schema

---
+++ src/models/User.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose"
+import { Schema } from "mongoose"
+
+export interface Iuser extends Document{
+    name: string,
+    email:  string,
+    password: string,
+    createdAt: Date,
+    updatedAt: Date
+}
+
+const userSchema = new Schema<Iuser>({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+},{
+    timestamps: true
+})
+
+const User = mongoose.model<Iuser>("User", userSchema);
+export default User;
