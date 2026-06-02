eef9b2702da200371785df9dbd9e5ff38bf66d63
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-06-02 23:54:35 +0530

    signup page

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,7 +1,15 @@
 export default function SignUp() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">Sign Up Page</h1>
+    <div className="flex h-screen">
+      <div className="w-2/4 max-h-full">
+      </div>
+
+      <div className="hidden md:block w-px bg-gray-300">
+      </div>
+
+      <div className="hidden md:block md:w-2/4">
+        <img src="/green.jpg" className="w-full h-full object-cover" />
+      </div>
     </div>
   );
 }
