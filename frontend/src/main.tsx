7014a9b06ca8bbea7833ead02b10ab868d8ba69a
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-06-08 23:59:18 +0530

    frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import SignUp from './pages/SignUp.tsx'
 import SignIn from "./pages/SignIn.tsx"
 import Dashboard from './pages/Dashboard.tsx'
-import CheckDemo from './pages/CheckDemo.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -16,7 +15,6 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/check-demo" element={<CheckDemo />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
--- frontend/src/types/index.ts
+++
@@ -1,7 +0,0 @@
-export interface ContentCard {
-  _id: string;
-  title: string;
-  type: "article" | "tweet" | "video" | "image" | "audio";
-  link: string;
-  tags: string[];
-}
