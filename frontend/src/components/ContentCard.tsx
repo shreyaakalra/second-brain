ce67cf5e9422c0b40ef62b4bb4097b39bdf10108
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-06-23 04:33:35 +0530

    bugs

--- frontend/src/components/ContentCard.tsx
+++ frontend/src/components/ContentCard.tsx
@@ -54,7 +54,7 @@ export default function ContentCard({card, onDelete}: ContentCardProps){
       {/* title */}
       <p className=" font-black text-black leading snug">{card.title}</p>
 
-      {card.previewImage && < />}
+      {card.previewImage && <img src={card.previewImage} />}
       
 
       {/* tags */}
