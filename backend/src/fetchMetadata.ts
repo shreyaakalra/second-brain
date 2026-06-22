00be1b740bf1455ed14477c8145f529538111893
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-06-23 03:35:57 +0530

    fetching meta data using cheerio

---
+++ backend/src/fetchMetadata.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+
+interface PreviewMetadata {
+  previewTitle?: string | undefined;
+  previewDescription?: string | undefined;
+  previewImage?: string | undefined;
+}
+
+export async function fetchMetadata(url: string): Promise<PreviewMetadata> {
+  try {
+    const response = await axios.get(url);
+
+    const $ = cheerio.load(response.data);
+
+    const previewTitle = $('meta[property="og:title"]').attr('content');
+    const previewDescription = $('meta[property="og:description"]').attr('content');
+    const previewImage = $('meta[property="og:image"]').attr('content');
+
+    return {
+        previewTitle,
+        previewDescription,
+        previewImage
+    }
+
+  } catch (err) {
+    console.log(err);
+    return {}; 
+  }
+}
